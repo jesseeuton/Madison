1bacb807daed9ab3b85b1476eeed7e8375c1ddfb
jesseeuton <jesseeuton@gmail.com>
2013-07-25 15:28:07 -0600

    DebtToIncome changes

--- Madison.Internal/Madison.Internal.QM.Web/Controllers/DebtToIncomeController.cs
+++ Madison.Internal/Madison.Internal.QM.Web/Controllers/DebtToIncomeController.cs
@@ -13,119 +13,43 @@ namespace Madison.Internal.QM.Web.Controllers
     {
         private TransactionContext db = new TransactionContext();
 
-        //
-        // GET: /DebtToIncome/
-
-        public ActionResult Index()
-        {
-            var debttoincome = db.DebtToIncome.Include(d => d.EmploymentStatus).Include(d => d.EmploymentLength).Include(d => d.EmploymentLengthAtPriorCompany);
-            return View(debttoincome.ToList());
-        }
-
-        //
-        // GET: /DebtToIncome/Details/5
-
-        public ActionResult Details(int id = 0)
-        {
-            DebtIncome debtincome = db.DebtToIncome.Find(id);
-            if (debtincome == null)
-            {
-                return HttpNotFound();
-            }
-            return View(debtincome);
-        }
-
-        //
-        // GET: /DebtToIncome/Create
-
-        public ActionResult Create()
+        public ActionResult DICalculator()
         {
             ViewBag.EmploymentStatusId = new SelectList(db.EmploymentStatus, "Id", "Name");
             ViewBag.EmploymentLengthId = new SelectList(db.EmploymentLength, "Id", "Name");
             ViewBag.EmploymentLengthAtPriorCompanyId = new SelectList(db.EmploymentLength, "Id", "Name");
+
             return View();
         }
 
-        //
-        // POST: /DebtToIncome/Create
-
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(DebtIncome debtincome)
+        public ActionResult DICalculator(DebtIncome debtIncome)
         {
-            if (ModelState.IsValid)
-            {
-                db.DebtToIncome.Add(debtincome);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
 
-            ViewBag.EmploymentStatusId = new SelectList(db.EmploymentStatus, "Id", "Name", debtincome.EmploymentStatusId);
-            ViewBag.EmploymentLengthId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthId);
-            ViewBag.EmploymentLengthAtPriorCompanyId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthAtPriorCompanyId);
-            return View(debtincome);
+
+            return RedirectToAction("Index", "Account");
         }
 
-        //
-        // GET: /DebtToIncome/Edit/5
+        
+        ////
+        //// POST: /DebtToIncome/Create
 
-        public ActionResult Edit(int id = 0)
-        {
-            DebtIncome debtincome = db.DebtToIncome.Find(id);
-            if (debtincome == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.EmploymentStatusId = new SelectList(db.EmploymentStatus, "Id", "Name", debtincome.EmploymentStatusId);
-            ViewBag.EmploymentLengthId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthId);
-            ViewBag.EmploymentLengthAtPriorCompanyId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthAtPriorCompanyId);
-            return View(debtincome);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(DebtIncome debtincome)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.DebtToIncome.Add(debtincome);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-        //
-        // POST: /DebtToIncome/Edit/5
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(DebtIncome debtincome)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(debtincome).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.EmploymentStatusId = new SelectList(db.EmploymentStatus, "Id", "Name", debtincome.EmploymentStatusId);
-            ViewBag.EmploymentLengthId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthId);
-            ViewBag.EmploymentLengthAtPriorCompanyId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthAtPriorCompanyId);
-            return View(debtincome);
-        }
-
-        //
-        // GET: /DebtToIncome/Delete/5
-
-        public ActionResult Delete(int id = 0)
-        {
-            DebtIncome debtincome = db.DebtToIncome.Find(id);
-            if (debtincome == null)
-            {
-                return HttpNotFound();
-            }
-            return View(debtincome);
-        }
-
-        //
-        // POST: /DebtToIncome/Delete/5
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            DebtIncome debtincome = db.DebtToIncome.Find(id);
-            db.DebtToIncome.Remove(debtincome);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //    ViewBag.EmploymentStatusId = new SelectList(db.EmploymentStatus, "Id", "Name", debtincome.EmploymentStatusId);
+        //    ViewBag.EmploymentLengthId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthId);
+        //    ViewBag.EmploymentLengthAtPriorCompanyId = new SelectList(db.EmploymentLength, "Id", "Name", debtincome.EmploymentLengthAtPriorCompanyId);
+        //    return View(debtincome);
+        //}
 
         protected override void Dispose(bool disposing)
         {
---
+++ Madison.Internal/Madison.Internal.QM.Web/ViewModels/DebtIncomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Madison.Internal.QM.Web.Models;
+
+namespace Madison.Internal.QM.Web.ViewModels
+{
+    public class DebtIncomeViewModel
+    {
+        public DebtIncome DebtIncome { get; set; }
+        public decimal TotalMonthlyIncome { get; set; }
+        public decimal TotalMonthlyObligations { get; set; }
+        public decimal TotalMonthlyObligationsAndPMI { get; set; }
+        public decimal DebtToIncomePercentage { get; set; }
+    }
+}
